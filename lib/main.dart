db641fbc5d17887af724608ae3919616ad93f21f
reemahmed <reemmahmed40@gmail.com>
2024-09-30 13:39:41 +0200

    init hive and notes box

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:app_notes/constants.dart';
 import 'package:app_notes/views/notes_view.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main()async {
+  await Hive.initFlutter();
+
+  await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
 }
 
