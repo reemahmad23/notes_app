cc6910a58067801a5131c98a0572e57b534f28d4
reemahmed <reemmahmed40@gmail.com>
2024-10-02 11:38:23 +0200

    format  current date

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -55,11 +55,15 @@ class _AddNoteFormState extends State<AddNoteForm> {
               onTap: () {
                 if (formKey.currentState!.validate()){
                   formKey.currentState!.save();
-            
+
+                  var currentDate = DateTime.now();
+                  var formattedCurrentDate = DateFormat.('dd-mm-yyyy')
+                  .format(currentDate);
+                  
                   var noteModel = NoteModel(
                     title: title!, 
                     subTitle: subTitle!, 
-                    date: DateTime.now().toString(), 
+                    date: formattedCurrentDate, 
                     color: Colors.blue.value);
             
                   BlocProvider.of<AddNotesCubit>(context).addNote(noteModel, context);  
